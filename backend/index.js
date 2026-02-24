a17a9eb86886d3e5a72e4951d38db228f1ced2d4
Naina Shukla <shuklanaina638@gmail.com>
2026-02-25 03:37:48 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173", 
-  "https://echobeats-project-z1m6.vercel.app"
+  "https://echobeats-project-z1m6.vercel.app",
 ];
 
 app.use(cors({
